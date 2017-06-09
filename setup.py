7c23ce2f302f15617defc053a1f6a6e9bc9238d6
lucamasera <lucamasera@users.noreply.github.com>
2017-06-09 10:16:22 +0200

    Added "license"

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+"""
+Author: Luca Masera.
+Edited from the PC++ project https://bitbucket.org/francesco-asnicar/gene_network_expansion.
+Copyright (C) 2017, all rights reserved
+This file (setup.py) is part of the PC++ project.
+PyPCalg is distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+"""
+
 from distutils.core import setup
 from distutils.extension import Extension
 
